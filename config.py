08ffce2bbdba8ba00f23868f4fc04d860039b848
InsulinusMaximus <pav.t2013@yandex.ru>
2022-07-10 22:53:24 +0300

    Fix some positions

--- config.py
+++ config.py
@@ -3,6 +3,11 @@ jenskay_odejda_urls = [
     f'https://search.wb.ru/exactmatch/ru/common/v4/search?appType=1&couponsGeo=2,12,7,3,6,18,22,21&curr=rub&dest=-1029256,-72180,-1777927,123586188&emp=0&lang=ru&locale=ru&page={page_num}&pricemarginCoeff=1.0&query=женская одежда&reg=0&regions=68,64,83,4,38,80,33,70,82,86,30,69,22,66,31,40,1,48&resultset=catalog&sort=popular&spp=0'
     for page_num in range(1, 100)]
 
+'''
+Velo 
+_______________________________________________________________________________________________
+'''
+
 query_sport_velo = 'СпортивныеВелосипедки'
 sport_velo_urls = [
     f'https://search.wb.ru/exactmatch/ru/common/v4/search?appType=1&couponsGeo=2,12,7,3,6,18,22,21&curr=rub&dest=-1029256,-72180,-1777927,123586188&emp=0&lang=ru&locale=ru&page={page_num}&pricemarginCoeff=1.0&query=%D1%81%D0%BF%D0%BE%D1%80%D1%82%D0%B8%D0%B2%D0%BD%D1%8B%D0%B5%20%D0%B2%D0%B5%D0%BB%D0%BE%D1%81%D0%B8%D0%BF%D0%B5%D0%B4%D0%BA%D0%B8&reg=0&regions=68,64,83,4,38,80,33,70,82,86,30,69,22,66,31,40,1,48&resultset=catalog&sort=popular&spp=0'
@@ -11,16 +16,26 @@ sport_velo_urls = [
 query_velo = 'велосипедки'
 velo_urls = [
     f'https://search.wb.ru/exactmatch/ru/common/v4/search?appType=1&couponsGeo=2,12,7,3,6,18,22,21&curr=rub&dest=-1029256,-72180,-1777927,123586188&emp=0&lang=ru&locale=ru&page={page_num}&pricemarginCoeff=1.0&query=велосипедки&reg=0&regions=68,64,83,4,38,80,33,70,82,86,30,69,22,66,31,40,1,48&resultset=catalog&sort=popular&spp=0'
-    for page_num in range(1, 100)]
+    for page_num in range(1, 61)]
 
 query_chernie_velo_jenskie = 'ВелосипедкиЖенскиеЧерные'
 chernie_velo_jenskie_urls = [
     f'https://search.wb.ru/exactmatch/ru/common/v4/search?appType=1&couponsGeo=2,12,7,3,6,18,22,21&curr=rub&dest=-1029256,-72180,-1777927,123586188&emp=0&lang=ru&locale=ru&page={page_num}&pricemarginCoeff=1.0&query=велосипедки женские черные&reg=0&regions=68,64,83,4,38,80,33,70,82,86,30,69,22,66,31,40,1,48&resultset=catalog&sort=popular&spp=0'
     for page_num in range(1, 61)]
 
+'''
+Topy 
+_______________________________________________________________________________________________
+'''
+
 query_beliy_top = 'БелыйТоп'
 beliy_top_urls = [
     f'https://search.wb.ru/exactmatch/ru/common/v4/search?appType=1&couponsGeo=2,12,7,3,6,18,22,21&curr=rub&dest=-1029256,-72180,-1777927,123586188&emp=0&lang=ru&locale=ru&page={page_num}&pricemarginCoeff=1.0&query=белый топ&reg=0&regions=68,64,83,4,38,80,33,70,82,86,30,69,22,66,31,40,1,48&resultset=catalog&sort=popular&spp=0'
+    for page_num in range(1, 61)]
+
+query_top_chernyy = 'ТопЧерный'
+top_chernyy_urls = [
+    f'https://search.wb.ru/exactmatch/ru/common/v4/search?appType=1&couponsGeo=2,12,7,3,6,18,22,21&curr=rub&dest=-1029256,-72180,-1777927,123586188&emp=0&lang=ru&locale=ru&page={page_num}&pricemarginCoeff=1.0&query=топ черный&reg=0&regions=68,64,83,4,38,80,33,70,82,86,30,69,22,66,31,40,1,48&resultset=catalog&sort=popular&spp=0'
     for page_num in range(1, 61)]
 
 query_topy_jenskye = 'ТопыЖенские'
@@ -37,3 +52,107 @@ query_topyky_jenskye = 'ТопикиЖенские'
 topyky_jenskye_urls = [
     f'https://search.wb.ru/exactmatch/ru/common/v4/search?appType=1&couponsGeo=2,12,7,3,6,18,22,21&curr=rub&dest=-1029256,-72180,-1777927,123586188&emp=0&lang=ru&locale=ru&page={page_num}&pricemarginCoeff=1.0&query=топики женские&reg=0&regions=68,64,83,4,38,80,33,70,82,86,30,69,22,66,31,40,1,48&resultset=catalog&sort=popular&spp=0'
     for page_num in range(1, 61)]
+
+query_zhenskiy_top_iz_hlopka = 'ТопЖенскийИзХлопка'
+zhenskiy_top_iz_hlopka_urls = [
+    f'https://search.wb.ru/exactmatch/ru/common/v4/search?appType=1&couponsGeo=2,12,7,3,6,18,22,21&curr=rub&dest=-1029256,-72180,-1777927,123586188&emp=0&lang=ru&locale=ru&page={page_num}&pricemarginCoeff=1.0&query=женский топ из хлопка&reg=0&regions=68,64,83,4,38,80,33,70,82,86,30,69,22,66,31,40,1,48&resultset=catalog&sort=popular&spp=0'
+    for page_num in range(1, 61)]
+
+query_top_zhenskiy_belyy_iz_khlopka = 'ТопЖенскийБелыйИзХлопка'
+zhenskiy_top_belyy_iz_hlopka_urls = [
+    f'https://search.wb.ru/exactmatch/ru/common/v4/search?appType=1&couponsGeo=2,12,7,3,6,18,22,21&curr=rub&dest=-1029256,-72180,-1777927,123586188&emp=0&lang=ru&locale=ru&page={page_num}&pricemarginCoeff=1.0&query=топ женский белый из хлопка&reg=0&regions=68,64,83,4,38,80,33,70,82,86,30,69,22,66,31,40,1,48&resultset=catalog&sort=popular&spp=0'
+    for page_num in range(1, 61)]
+
+
+'''
+Bruky
+_______________________________________________________________________________________________
+'''
+
+query_shtany = 'Штаны'
+shtany_urls = [
+    f'https://search.wb.ru/exactmatch/ru/common/v4/search?appType=1&couponsGeo=2,12,7,3,6,18,22,21&curr=rub&dest=-1029256,-72180,-1777927,123586188&emp=0&lang=ru&locale=ru&page={page_num}&pricemarginCoeff=1.0&query=штаны&reg=0&regions=68,64,83,4,38,80,33,70,82,86,30,69,22,66,31,40,1,48&resultset=catalog&sort=popular&spp=0'
+    for page_num in range(1, 61)]
+
+query_bruky_jenskye_letnye = 'БрюкиЖенскиеЛетние'
+bruky_jenskye_letnye_urls = [
+    f'https://search.wb.ru/exactmatch/ru/common/v4/search?appType=1&couponsGeo=2,12,7,3,6,18,22,21&curr=rub&dest=-1029256,-72180,-1777927,123586188&emp=0&lang=ru&locale=ru&page={page_num}&pricemarginCoeff=1.0&query=брюки женские летние&reg=0&regions=68,64,83,4,38,80,33,70,82,86,30,69,22,66,31,40,1,48&resultset=catalog&sort=popular&spp=0'
+    for page_num in range(1, 61)]
+
+query_bruky = 'Брюки'
+bruky_urls = [
+    f'https://search.wb.ru/exactmatch/ru/common/v4/search?appType=1&couponsGeo=2,12,7,3,6,18,22,21&curr=rub&dest=-1029256,-72180,-1777927,123586188&emp=0&lang=ru&locale=ru&page={page_num}&pricemarginCoeff=1.0&query=брюки&reg=0&regions=68,64,83,4,38,80,33,70,82,86,30,69,22,66,31,40,1,48&resultset=catalog&sort=popular&spp=0'
+    for page_num in range(1, 61)]
+
+query_bruky_ykorochenie = 'БрюкиУкороченные'
+ykorochenie_bruky_urls = [
+    f'https://search.wb.ru/exactmatch/ru/common/v4/search?appType=1&couponsGeo=2,12,7,3,6,18,22,21&curr=rub&dest=-1029256,-72180,-1777927,123586188&emp=0&lang=ru&locale=ru&page={page_num}&pricemarginCoeff=1.0&query=брюки&reg=0&regions=68,64,83,4,38,80,33,70,82,86,30,69,22,66,31,40,1,48&resultset=catalog&sort=popular&spp=0'
+    for page_num in range(1, 61)]
+
+query_bryuki_zhenskiye_bezhevyye = 'БрюкиЖенскиеБежевые'
+bryuki_zhenskiye_bezhevyye_urls = [
+    f'https://search.wb.ru/exactmatch/ru/common/v4/search?appType=1&couponsGeo=2,12,7,3,6,18,22,21&curr=rub&dest=-1029256,-72180,-1777927,123586188&emp=0&lang=ru&locale=ru&page={page_num}&pricemarginCoeff=1.0&query=брюки женские бежевые&reg=0&regions=68,64,83,4,38,80,33,70,82,86,30,69,22,66,31,40,1,48&resultset=catalog&sort=popular&spp=0'
+    for page_num in range(1, 61)]
+
+'''
+Futbolka
+_______________________________________________________________________________________________
+'''
+
+query_futbolka_zhenskaya_belaya_s_printom = 'ФутболкаЖенскаяБелаяСПринтом'
+futbolka_zhenskaya_belaya_s_printom_urls = [
+    f'https://search.wb.ru/exactmatch/ru/common/v4/search?appType=1&couponsGeo=2,12,7,3,6,18,22,21&curr=rub&dest=-1029256,-72180,-1777927,123586188&emp=0&lang=ru&locale=ru&page={page_num}&pricemarginCoeff=1.0&query=футболка женская белая с принтом&reg=0&regions=68,64,83,4,38,80,33,70,82,86,30,69,22,66,31,40,1,48&resultset=catalog&sort=popular&spp=0'
+    for page_num in range(1, 61)]
+
+query_ukorochennaya_zhenskaya_futbolka_iz_khlopka = 'УкороченнаяЖенскаяФутболкаИзХлопка'
+ukorochennaya_zhenskaya_futbolka_iz_khlopkaurls = [
+    f'https://search.wb.ru/exactmatch/ru/common/v4/search?appType=1&couponsGeo=2,12,7,3,6,18,22,21&curr=rub&dest=-1029256,-72180,-1777927,123586188&emp=0&lang=ru&locale=ru&page={page_num}&pricemarginCoeff=1.0&query=укороченная женская футболка из хлопка&reg=0&regions=68,64,83,4,38,80,33,70,82,86,30,69,22,66,31,40,1,48&resultset=catalog&sort=popular&spp=0'
+    for page_num in range(1, 61)]
+
+
+'''
+Pizhamy
+_______________________________________________________________________________________________
+'''
+
+query_pizhamy_zhenskiye_s_shortami = 'ПижамыЖенскиеСШортами'
+pizhamy_zhenskiye_s_shortami_urls = [
+    f'https://search.wb.ru/exactmatch/ru/common/v4/search?appType=1&couponsGeo=2,12,7,3,6,18,22,21&curr=rub&dest=-1029256,-72180,-1777927,123586188&emp=0&lang=ru&locale=ru&page={page_num}&pricemarginCoeff=1.0&query=пижамы женские с шортами&reg=0&regions=68,64,83,4,38,80,33,70,82,86,30,69,22,66,31,40,1,48&resultset=catalog&sort=popular&spp=0'
+    for page_num in range(1, 70)]
+
+query_pizhama_zhenskaya_s_shortami = 'ПижамаЖенскиеСШортами'
+pizhama_zhenskaya_s_shortami_urls = [
+    f'https://search.wb.ru/exactmatch/ru/common/v4/search?appType=1&couponsGeo=2,12,7,3,6,18,22,21&curr=rub&dest=-1029256,-72180,-1777927,123586188&emp=0&lang=ru&locale=ru&page={page_num}&pricemarginCoeff=1.0&query=Пижама женская с шортами&reg=0&regions=68,64,83,4,38,80,33,70,82,86,30,69,22,66,31,40,1,48&resultset=catalog&sort=popular&spp=0'
+    for page_num in range(1, 70)]
+
+query_pizhama_zhenskaya_s_shortami_golubaya = 'ПижамаЖенскаяСШортамиГолубая'
+pizhama_zhenskaya_s_shortami_golubaya_urls = [
+    f'https://search.wb.ru/exactmatch/ru/common/v4/search?appType=1&couponsGeo=2,12,7,3,6,18,22,21&curr=rub&dest=-1029256,-72180,-1777927,123586188&emp=0&lang=ru&locale=ru&page={page_num}&pricemarginCoeff=1.0&query=Пижама женская с шортами голубая&reg=0&regions=68,64,83,4,38,80,33,70,82,86,30,69,22,66,31,40,1,48&resultset=catalog&sort=popular&spp=0'
+    for page_num in range(1, 70)]
+
+'''
+Sorochky
+_______________________________________________________________________________________________
+'''
+
+
+
+'''
+Tolstovky
+_______________________________________________________________________________________________
+'''
+query_tolstovka_zhenskaya_bez_kapyushona = 'ТолстовкаЖенскаяБезКапюшона'
+tolstovka_zhenskaya_bez_kapyushona_urls = [
+    f'https://search.wb.ru/exactmatch/ru/common/v4/search?appType=1&couponsGeo=2,12,7,3,6,18,22,21&curr=rub&dest=-1029256,-72180,-1777927,123586188&emp=0&lang=ru&locale=ru&page={page_num}&pricemarginCoeff=1.0&query=толстовка женская без капюшона&reg=0&regions=68,64,83,4,38,80,33,70,82,86,30,69,22,66,31,40,1,48&resultset=catalog&sort=popular&spp=0'
+    for page_num in range(1, 70)]
+
+query_khudi_zhenskoye_oversayz_sirenevoye = 'ХудиЖенскоеОверсайзСиреневое'
+khudi_zhenskoye_oversayz_sirenevoye_urls = [
+    f'https://search.wb.ru/exactmatch/ru/common/v4/search?appType=1&couponsGeo=2,12,7,3,6,18,22,21&curr=rub&dest=-1029256,-72180,-1777927,123586188&emp=0&lang=ru&locale=ru&page={page_num}&pricemarginCoeff=1.0&query=Худи женское оверсайз сиреневое&reg=0&regions=68,64,83,4,38,80,33,70,82,86,30,69,22,66,31,40,1,48&resultset=catalog&sort=popular&spp=0'
+    for page_num in range(1, 70)]
+
+query_tolstovka_zhenskaya_s_kapyushonom_rozovaya = 'ТолстовкаЖенскаяСКапюшономРозовая'
+tolstovka_zhenskaya_s_kapyushonom_rozovaya_urls = [
+    f'https://search.wb.ru/exactmatch/ru/common/v4/search?appType=1&couponsGeo=2,12,7,3,6,18,22,21&curr=rub&dest=-1029256,-72180,-1777927,123586188&emp=0&lang=ru&locale=ru&page={page_num}&pricemarginCoeff=1.0&query=Толстовка женская с капюшоном розовая&reg=0&regions=68,64,83,4,38,80,33,70,82,86,30,69,22,66,31,40,1,48&resultset=catalog&sort=popular&spp=0'
+    for page_num in range(1, 70)]
+
--- main.py
+++ main.py
@@ -5,7 +5,47 @@ import config
 if __name__ == '__main__':
 
     parser = parser.Parser_WB()
-    parser.run(config.topyky_jenskye_urls, config.query_topyky_jenskye)
+
+    # Velo
+
+    #parser.run(config.velo_urls, config.query_velo)
+    parser.run(config.sport_velo_urls, config.query_sport_velo)
+
+    # Top
+
+    #parser.run(config.beliy_top_urls, config.query_beliy_top)
+    #parser.run(config.top_chernyy_urls, config.query_top_chernyy)
+    #parser.run(config.topyky_jenskye_urls, config.query_topyky_jenskye)
+    #parser.run(config.top_sportyvniy_urls, config.query_top_sportyvniy)
+    #parser.run(config.zhenskiy_top_iz_hlopka_urls, config.query_zhenskiy_top_iz_hlopka)
+    #parser.run(config.zhenskiy_top_belyy_iz_hlopka_urls, config.query_top_zhenskiy_belyy_iz_khlopka)
+
+    # Bruky
+
+    #parser.run(config.shtany_urls, config.query_shtany)
+    #parser.run(config.bruky_jenskye_letnye_urls, config.query_bruky_jenskye_letnye)
+    #parser.run(config.bruky_urls, config.query_bruky)
+    #parser.run(config.ykorochenie_bruky_urls, config.query_bruky_ykorochenie)
+    #parser.run(config.bryuki_zhenskiye_bezhevyye_urls, config.query_bryuki_zhenskiye_bezhevyye)
+
+    # Futbolka
+
+    #parser.run(config.futbolka_zhenskaya_belaya_s_printom_urls, config.query_futbolka_zhenskaya_belaya_s_printom)
+    #parser.run(config.ukorochennaya_zhenskaya_futbolka_iz_khlopkaurls, config.query_ukorochennaya_zhenskaya_futbolka_iz_khlopka)
+
+    # Pizhamy
+
+    #parser.run(config.pizhamy_zhenskiye_s_shortami_urls, config.query_pizhamy_zhenskiye_s_shortami)
+    #parser.run(config.pizhama_zhenskaya_s_shortami_urls, config.query_pizhama_zhenskaya_s_shortami)
+    #parser.run(config.pizhama_zhenskaya_s_shortami_golubaya_urls, config.query_pizhama_zhenskaya_s_shortami_golubaya)
+
+    # Sorochky
+
+    # Tolstovky
+
+    #parser.run(config.tolstovka_zhenskaya_bez_kapyushona_urls, config.query_tolstovka_zhenskaya_bez_kapyushona)
+    #parser.run(config.khudi_zhenskoye_oversayz_sirenevoye_urls, config.query_khudi_zhenskoye_oversayz_sirenevoye)
+    #parser.run(config.tolstovka_zhenskaya_s_kapyushonom_rozovaya_urls, config.query_tolstovka_zhenskaya_s_kapyushonom_rozovaya)
 
     for data in parser.result:
         print(data)
